dbcd3c993174eef75efac2c8a5ee99890c318504
Chris Waldon <christopher.waldon.dev@gmail.com>
2017-09-23 17:42:17 -0400

    Update default font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+char font[] = "Ubuntu Mono:size=12:antialias=true:autohint=true";
 int borderpx = 2;
 
 /*
