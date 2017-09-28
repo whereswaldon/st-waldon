e96d12af9f8a595596d061d26bc278c040f33cab
Chris Waldon <christopher.waldon.dev@gmail.com>
2017-09-28 09:23:18 -0400

    More transparency

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ char termname[] = "st-256color";
 static unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xcc;
+unsigned int alpha = 0xaa;
 
 /* Terminal colors (16 first used in escape sequence) */
 const char *colorname[] = {
